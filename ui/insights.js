079228ebfb631bc2a0b007aa5cd93f171e6fd583
Joshan Randhawa <joshanrandhawa5@hotmail.com>
2026-06-19 18:46:42 +0100

    feat(ui): add preload.js contextBridge IPC bridge and insights.js stub

---
+++ ui/insights.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// Node-side insight engine. Populated in Phase 4.
+// Returns [] until the insight functions are implemented.
+
+function getInsights(dbPath, max = 2) {
+  return []
+}
+
+module.exports = { getInsights }
---
+++ ui/preload.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('klokd', {
+  getTodaySummary: () => ipcRenderer.invoke('klokd:getTodaySummary'),
+  getTopApps: () => ipcRenderer.invoke('klokd:getTopApps'),
+  getWeekTrends: () => ipcRenderer.invoke('klokd:getWeekTrends'),
+  getInsights: () => ipcRenderer.invoke('klokd:getInsights'),
+  getAutoLaunchEnabled: () => ipcRenderer.invoke('klokd:getAutoLaunchEnabled'),
+  setAutoLaunch: (enabled) => ipcRenderer.invoke('klokd:setAutoLaunch', enabled),
+  getSettings: () => ipcRenderer.invoke('klokd:getSettings'),
+  setPollInterval: (seconds) => ipcRenderer.invoke('klokd:setPollInterval', seconds),
+  getCategoriesYaml: () => ipcRenderer.invoke('klokd:getCategoriesYaml'),
+  setCategoriesYaml: (content) => ipcRenderer.invoke('klokd:setCategoriesYaml', content),
+  recategoriseAll: () => ipcRenderer.invoke('klokd:recategoriseAll'),
+  exportData: () => ipcRenderer.invoke('klokd:exportData'),
+  deleteAllData: () => ipcRenderer.invoke('klokd:deleteAllData'),
+})
